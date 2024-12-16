1975f53932877318f459d1dc98dcf7211e5491f8
Abay Raju M <67160846+reach-abay@users.noreply.github.com>
2024-12-16 15:48:25 +0530

    Auth + token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Home from './pages/Home';
+import MusicHome from './pages/MusicHome';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
     <Routes>       
       <Route path="/" element={<Home />} />
+      <Route path="/Musichome" element={<MusicHome />} />
     </Routes>
   </BrowserRouter>
   );
---
+++ src/pages/MusicHome.js
@@ -0,0 +1,3 @@
+export default function MusicHome() {
+    return (<div>Music HOme</div>)
+}
